2e8ec30006701693c29b2cb167bbd8c681ce0a5f
pefilippov <pefilippov@ozon.ru>
2020-06-27 15:57:15 +0300

    creating wrapper to create first state

--- 04_scope/02_block-scope/02_closure/04/main.go
+++ 04_scope/02_block-scope/02_closure/04/main.go
@@ -9,8 +9,25 @@ func wrapper() func() int {
 		return x
 	}
 }
+func wrapperFib() func() int {
+	a, b := 0, 1
+	return func() int {
+		a, b = b, a
+		b = a + b
+		return a
+	}
+}
 func main() {
 	increment := wrapper()
 	fmt.Println(increment())
 	fmt.Println(increment())
+	fib := wrapperFib()
+	fmt.Println(fib())
+	fmt.Println(fib())
+	fmt.Println(fib())
+	fmt.Println(fib())
+	fmt.Println(fib())
+	fmt.Println(fib())
+	fmt.Println(fib())
+	fmt.Println(fib())
 }
